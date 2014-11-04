efe94d2cae562846d057e8dfe1e36948836a6cbb
emccallum <mccallum.ek@gmail.com>
2014-11-04 13:32:35 -0800

    Fixing todos representation to model domain

--- src/main/scala/emccallum/representations/TodosRepresentation.scala
+++ src/main/scala/emccallum/representations/TodosRepresentation.scala
@@ -1,6 +1,9 @@
 package emccallum.representations
 
-import emccallum.model.Todo
 import com.fasterxml.jackson.annotation.JsonProperty
+import emccallum.model.Todo
 
-case class TodosRepresentation(@JsonProperty("todos") todos: Seq[Todo])
+case class TodosRepresentation(private val todos: Seq[Todo]) {
+  @JsonProperty("todos")
+  lazy val todoRepresentations = todos.map(TodoRepresentation)
+}
